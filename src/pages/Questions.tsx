857dcc7425bb9558aadbb14b1f78c1c7ad5e66b7
Yin <yinyin.k@rznet.com>
2024-09-09 22:58:34 +0700

    success page added

--- src/pages/Questions.tsx
+++ src/pages/Questions.tsx
@@ -19,9 +19,7 @@ const Questions: React.FC = () => {
   ];
 
   const handleNext = () => {
-    if (currentStep < 2) {
-      setCurrentStep((prev) => prev + 1);
-    }
+    setCurrentStep((prev) => prev + 1);
   };
 
   const handlePrevious = () => {
@@ -34,51 +32,58 @@ const Questions: React.FC = () => {
     <div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-100">
       <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg">
         {/* Stepper Header */}
-        <div className="flex justify-between mb-6">
-          {steps.map((step) => (
-            <div
-              key={step.id}
-              className={`w-full p-2 text-center border-b-4 ${
-                currentStep === step.id ? "border-blue-500" : "border-gray-300"
-              }`}
-            >
-              {step.name}
+
+        {currentStep <= steps.length ? (
+          <>
+            <div className="flex justify-between mb-6">
+              {steps.map((step) => (
+                <div
+                  key={step.id}
+                  className={`w-full p-2 text-center border-b-4 ${
+                    currentStep === step.id
+                      ? "border-blue-500"
+                      : "border-gray-300"
+                  }`}
+                >
+                  {step.name}
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
 
-        {/* Stepper Content */}
-        <div className="mb-4">
-          <div className="relative sm:max-w-[300px] md:max-w-[400px] lg:max-w-[500px] mx-auto h-[auto] flex items-center justify-center">
-            {steps[currentStep - 1].content}
+            {/* Stepper Content */}
+            <div className="mb-4">
+              <div className="relative sm:max-w-[300px] md:max-w-[400px] lg:max-w-[500px] mx-auto h-[auto] flex items-center justify-center">
+                {steps[currentStep - 1].content}
+              </div>
+            </div>
+
+            {/* Stepper Controls */}
+            <div className="flex justify-between">
+              <button
+                onClick={handlePrevious}
+                disabled={currentStep === 1}
+                className={`px-4 py-2 rounded ${
+                  currentStep === 1
+                    ? "bg-gray-300 text-gray-600 cursor-not-allowed"
+                    : "bg-blue-500 text-white"
+                }`}
+              >
+                Previous
+              </button>
+              <button
+                onClick={handleNext}
+                className={`px-4 py-2 rounded  bg-blue-500 text-white`}
+              >
+                {currentStep === 2 ? "Submit" : "Next"}
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className="p-4 text-center">
+            <h2 className="mb-4 text-2xl font-bold">Successfully Completed!</h2>
+            <p>Thanks for your time.</p>
           </div>
-        </div>
-
-        {/* Stepper Controls */}
-        <div className="flex justify-between">
-          <button
-            onClick={handlePrevious}
-            disabled={currentStep === 1}
-            className={`px-4 py-2 rounded ${
-              currentStep === 1
-                ? "bg-gray-300 text-gray-600 cursor-not-allowed"
-                : "bg-blue-500 text-white"
-            }`}
-          >
-            Previous
-          </button>
-          <button
-            onClick={handleNext}
-            disabled={currentStep === 2}
-            className={`px-4 py-2 rounded ${
-              currentStep === 2
-                ? "bg-gray-300 text-gray-600 cursor-not-allowed"
-                : "bg-blue-500 text-white"
-            }`}
-          >
-            Next
-          </button>
-        </div>
+        )}
       </div>
     </div>
   );
